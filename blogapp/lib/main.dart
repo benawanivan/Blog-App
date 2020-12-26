7cf194e98e76a54f7481a8a62d39fa2511b6ff07
BenawanIvan <benawan.ivan@gmail.com>
2020-12-26 21:56:37 +0700

    draft1

--- blogapp/lib/main.dart
+++ blogapp/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import './post.dart' as post;
-import './kategori.dart' as kategori;
-import './search.dart' as search;
 
 void main() {
   runApp(new MaterialApp(
@@ -38,28 +35,107 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
           child: Text('UAS PBP Kelompok 7'),
         ),
         backgroundColor: Colors.teal[300],
-        bottom: new TabBar(
-          controller: controller,
-          tabs: <Widget>[
-            new Tab(
-              icon: new Text("Post"),
-            ),
-            new Tab(
-              icon: new Text("Kategori"),
-            ),
-            new Tab(
-              icon: new Text("Search"),
-            ),
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Search(),
+              new Post(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Post extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Cards(),
           ],
         ),
       ),
-      body: new TabBarView(
-        controller: controller,
-        children: <Widget>[
-          new post.Post(),
-          new kategori.Kategori(),
-          new search.Search(),
-        ],
+    );
+  }
+}
+
+class Cards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            onPressed: () {},
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new Image.asset("img/bromo.jpg"),
+                new Padding(padding: new EdgeInsets.all(5.0)),
+                new Text("Sample Tanggal",
+                    style: new TextStyle(
+                        fontSize: 12.0,
+                        fontFamily: "Delicious",
+                        color: Colors.grey)),
+                new Padding(padding: new EdgeInsets.all(5.0)),
+                new Text("Sample Judul",
+                    style: new TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: "Delicious",
+                        color: Colors.black)),
+                new Padding(padding: new EdgeInsets.all(5.0)),
+                new Text("Sample Penulis",
+                    style: new TextStyle(
+                        fontSize: 12.0,
+                        fontFamily: "Delicious",
+                        color: Colors.grey)),
+                new Padding(padding: new EdgeInsets.all(3.0)),
+                new Text(
+                    "Sample Post Sample Post Sample Post Sample Post Sample Post Sample Post Sample Post",
+                    style: new TextStyle(
+                        fontSize: 12.0,
+                        fontFamily: "Delicious",
+                        color: Colors.grey)),
+                new Padding(padding: new EdgeInsets.all(5.0)),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class Search extends StatefulWidget {
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  String teks = "";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new TextField(
+        style: TextStyle(fontSize: 20),
+        decoration: new InputDecoration(
+          hintText: "Search",
+          hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+        ),
+        onSubmitted: (String str) {
+          setState(() {
+            teks = str;
+          });
+        },
       ),
     );
   }
